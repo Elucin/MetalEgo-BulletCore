6e6b632f6380b3e7abfeb4c341457521ac8ebdb5
Andrew Smith <ajsmith22@live.com>
2016-11-11 16:47:00 -0500

    Targeting

--- METAL EGO Bullet Core/Assets/Scripts/Game/ControlDebug.cs
+++ METAL EGO Bullet Core/Assets/Scripts/Game/ControlDebug.cs
@@ -12,39 +12,49 @@ public class ControlDebug : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetButtonDown("j1_b0"))
-			Debug.Log("Right - Button 0");
-		if(Input.GetButtonDown("j1_b1"))
-			Debug.Log("Right - Button 1");
-		if(Input.GetButtonDown("j1_b2"))
-			Debug.Log("Right - Button 2");
-		if(Input.GetButtonDown("j1_b3"))
-			Debug.Log("Right - Button 3");
-		if(Input.GetButtonDown("j1_b4"))
-			Debug.Log("Right - Button 4");
-		if(Input.GetButtonDown("j1_b5"))
-			Debug.Log("Right - Button 5");
-		
-		if(Input.GetButtonDown("j2_b0"))
-			Debug.Log("Left - Button 0");
-		if(Input.GetButtonDown("j2_b1"))
-			Debug.Log("Left - Button 1");
-		if(Input.GetButtonDown("j2_b2"))
-			Debug.Log("Left - Button 2");
-		if(Input.GetButtonDown("j2_b3"))
-			Debug.Log("Left - Button 3");
-		if(Input.GetButtonDown("j2_b4"))
-			Debug.Log("Left - Button 4");
-		if(Input.GetButtonDown("j2_b5"))
-			Debug.Log("Left - Button 5");
+	void LateUpdate () {
+		DebugJoy1 ();
+		DebugJoy2 ();
+		DebugJoy3 ();	
+	}
+
+	void DebugJoy1()
+	{
+		if (Input.GetButtonDown ("j1_b0"))
+			Debug.Log ("Right - Button 0");
+		if (Input.GetButtonDown ("j1_b1"))
+			Debug.Log ("Right - Button 1");
+		if (Input.GetButtonDown ("j1_b2"))
+			Debug.Log ("Right - Button 2");
+		if (Input.GetButtonDown ("j1_b3"))
+			Debug.Log ("Right - Button 3");
+		if (Input.GetButtonDown ("j1_b4"))
+			Debug.Log ("Right - Button 4");
+		if (Input.GetButtonDown ("j1_b5"))
+			Debug.Log ("Right - Button 5");
 
 		if (Mathf.Abs(Input.GetAxis ("j1_X")) > 0.05f)
 			Debug.Log ("Right - X Axis");
 		if (Mathf.Abs(Input.GetAxis ("j1_Y")) > 0.05f)
 			Debug.Log ("Right - Y Axis");
 		if (Mathf.Abs(Input.GetAxis ("j1_Z")) > 0.05f)
-			Debug.Log ("Right - Z Axis");
+			Debug.Log ("Right - Z Axis");	
+	}
+
+	void DebugJoy2()
+	{
+		if (Input.GetButtonDown ("j2_b0"))
+			Debug.Log ("Left - Button 0");
+		if (Input.GetButtonDown ("j2_b1"))
+			Debug.Log ("Left - Button 1");
+		if (Input.GetButtonDown ("j2_b2"))
+			Debug.Log ("Left - Button 2");
+		if (Input.GetButtonDown ("j2_b3"))
+			Debug.Log ("Left - Button 3");
+		if (Input.GetButtonDown ("j2_b4"))
+			Debug.Log ("Left - Button 4");
+		if (Input.GetButtonDown ("j2_b5"))
+			Debug.Log ("Left - Button 5");
 
 		if (Mathf.Abs(Input.GetAxis ("j2_X")) > 0.05f)
 			Debug.Log ("Left - X Axis");
@@ -52,10 +62,14 @@ public class ControlDebug : MonoBehaviour {
 			Debug.Log ("Left - Y Axis");
 		if (Mathf.Abs(Input.GetAxis ("j2_Z")) > 0.05f)
 			Debug.Log ("Left - Z Axis: " + Input.GetAxis("j2_Z"));
+		
+	}
 
+	void DebugJoy3()
+	{
 		if (Input.GetAxis ("j3_X") > 0.05f)
 			Debug.Log ("Pedals - Left Foot");
-		if (Input.GetAxis ("j3_Y") >  0.05f)
+		if (Input.GetAxis ("j3_Y") > 0.05f)
 			Debug.Log ("Pedals - Right Foot");
 		if (Input.GetAxis ("j3_Z") > 0.05f)
 			Debug.Log ("Pedals - Right Forward");
--- METAL EGO Bullet Core/Assets/Scripts/Player/PlayerControl.cs
+++ METAL EGO Bullet Core/Assets/Scripts/Player/PlayerControl.cs
@@ -7,6 +7,14 @@ public class PlayerControl : MonoBehaviour {
 	const float SPEED_DENOM = 0.075f;
 	const float MAX_SPEED = 10f;
 	const float MAX_TURN_SPEED = 1f;
+	const int TARGET_SMOOTHING = 10;
+	public GameObject RightPivot;
+	public GameObject LeftPivot;
+
+	System.Collections.Generic.List<float> j1_xAxis;
+	System.Collections.Generic.List<float> j1_yAxis;
+	System.Collections.Generic.List<float> j2_xAxis;
+	System.Collections.Generic.List<float> j2_yAxis;
 
 	System.Collections.Generic.List<float> speeds;
 	float oldAxisPos;
@@ -18,12 +26,36 @@ public class PlayerControl : MonoBehaviour {
 		rigidBody = GetComponent<Rigidbody> ();
 		oldAxisPos = 0f;
 		speeds = new System.Collections.Generic.List<float> ();
+		j1_xAxis = new System.Collections.Generic.List<float> ();
+		j1_yAxis = new System.Collections.Generic.List<float> ();
+		j2_xAxis = new System.Collections.Generic.List<float> ();
+		j2_yAxis = new System.Collections.Generic.List<float> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		j1_xAxis.Add(Input.GetAxis ("j1_X"));
+		j1_yAxis.Add(Input.GetAxis ("j1_Y"));
+		j2_xAxis.Add(Input.GetAxis ("j2_X"));
+		j2_yAxis.Add(Input.GetAxis ("j2_Y"));
+		if (j1_xAxis.Count > TARGET_SMOOTHING)
+			j1_xAxis.RemoveAt (0);
+		if (j1_yAxis.Count > TARGET_SMOOTHING)
+			j1_yAxis.RemoveAt (0);
+		if (j2_xAxis.Count > TARGET_SMOOTHING)
+			j2_xAxis.RemoveAt (0);
+		if (j2_yAxis.Count > TARGET_SMOOTHING)
+			j2_yAxis.RemoveAt (0);
+		
 		Movement ();
-		//speedCoefficient = GetSpeedCoefficient ();
+		Targeting ();
+		//CrouchAndJump ();
+	}
+
+	void FixedUpdate()
+	{
+		
 	}
 
 	float GetSpeedCoefficient()
@@ -49,19 +81,49 @@ public class PlayerControl : MonoBehaviour {
 
 	float GetTurnCoefficient()
 	{
-		Debug.Log ((1 + Input.GetAxis("j3_X")) - (1 + Input.GetAxis("j3_Y")));
 		return (1 + Input.GetAxis("j3_X")) - (1 + Input.GetAxis("j3_Y"));
-		/*
-		if (Input.GetAxis ("j3_X") > Input.GetAxis ("j3_Y"))
-			return Input.GetAxis ("j3_X");
-		else if (Input.GetAxis("j3_X") < Input.GetAxis("j3_Y"))
-			return Input.GetAxis ("j3_X"); */
-
 	}
 
 	void Movement()
 	{
 		rigidBody.velocity = transform.TransformDirection(new Vector3 (0, 0, MAX_SPEED) * GetSpeedCoefficient ());
-		rigidBody.angularVelocity = new Vector3 (0, -GetTurnCoefficient () / 2, 0);
+		if (Input.GetAxis ("j3_X") > -0.9f && Input.GetAxis ("j3_Y") > -0.9f && Mathf.Abs (GetTurnCoefficient ()) < 0.5f) {
+			Camera.main.transform.localPosition = Vector3.Lerp (Camera.main.transform.localPosition, new Vector3 (0, -0.2f * (Input.GetAxis ("j3_X") + Input.GetAxis ("j3_Y") / 2f), Camera.main.transform.localPosition.z), Time.time / 100f); 
+		} else {
+			Camera.main.transform.localPosition = Vector3.Lerp (Camera.main.transform.localPosition, new Vector3 (0, (1 - Mathf.Abs (Input.GetAxis ("j3_Z")) * 0.2f), Camera.main.transform.localPosition.z), Time.time / 50f); //Head Bob
+			rigidBody.angularVelocity = new Vector3 (0, -GetTurnCoefficient () / 2, 0);
+		}
+		transform.localEulerAngles = new Vector3 (0, transform.localEulerAngles.y, 0);
+	}
+
+	void Targeting()
+	{
+		/*
+		RightPivot.transform.localEulerAngles += new Vector3 (j1_yAxis, j1_xAxis, 0);
+		RightPivot.transform.localEulerAngles = new Vector3(Mathf.Clamp(RightPivot.transform.localEulerAngles.x, 300f, 358.9f), Mathf.Clamp(RightPivot.transform.localEulerAngles.y, 1f, 90f), RightPivot.transform.localEulerAngles.z);
+		LeftPivot.transform.localEulerAngles += new Vector3 (j2_yAxis, j2_xAxis, 0);
+		LeftPivot.transform.localEulerAngles = new Vector3(Mathf.Clamp(LeftPivot.transform.localEulerAngles.x, 300f, 358.9f), Mathf.Clamp(LeftPivot.transform.localEulerAngles.y, 270f, 358.9f), LeftPivot.transform.localEulerAngles.z);
+		*/
+
+
+		RightPivot.transform.localEulerAngles = new Vector3 (35 * GetFloatListAverage(j1_yAxis), 45 + 45 *  GetFloatListAverage(j1_xAxis), 0);
+		LeftPivot.transform.localEulerAngles = new Vector3 (35 * GetFloatListAverage(j2_yAxis), 315 + 45 * GetFloatListAverage(j2_xAxis), 0);
+	}
+
+	float GetFloatListAverage(System.Collections.Generic.List<float> list)
+	{
+		float avg = 0f;
+
+		foreach (float f in list) {
+			avg += f;
+		}
+
+		avg = avg / list.Count;
+		return avg;
+	}
+
+	void CrouchAndJump()
+	{
+		
 	}
 }
