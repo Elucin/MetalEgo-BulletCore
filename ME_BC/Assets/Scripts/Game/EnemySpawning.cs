66703631d4bd57ea5451412b97160bea3c7c8f31
Travis Swan <t_r_z_swan@hotmail.com>
2016-12-01 17:08:06 -0500

    Changes to enemy mech prefab

--- ME_BC/Assets/Scripts/Game/EnemySpawning.cs
+++ ME_BC/Assets/Scripts/Game/EnemySpawning.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class EnemySpawning : MonoBehaviour {
 
+	public PlayerHealth playerHealth;
+	public GameObject enemy;
+	public float spawnTime = 3f;
+	public Transform[] spawnPoints;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepearing ("Spawn", spawnTime, spawnTime);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Spawn () {
+		if (playerHealth.currentHealth <= 0)
+			return;
+
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
 	}
 }
