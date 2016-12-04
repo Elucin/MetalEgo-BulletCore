213bd5c96d85e66ab821799ee8bfcf2df8bd869a
Travis Swan <t_r_z_swan@hotmail.com>
2016-12-03 19:36:58 -0500

    Added script to target the player at all times

---
+++ ME_BC/Assets/Scripts/AI/LookAtPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+	private GameObject player;
+
+	void Start(){
+		player = GameObject.FindWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(player.transform.position);
+	}
+}
